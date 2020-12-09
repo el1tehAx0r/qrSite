b94900f263a283d80e8e8c25560e5288b335e4f8
el1tehax0r <professionalstuff83@gmail.com>
2020-12-09 03:40:38 -0600

    once more for publish

--- src/FormComponents/Button.js
+++ src/FormComponents/Button.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../Stylesheet'
 export default function Button({onClick,title}){
+  //
   return (<button style={styles.button} onClick={onClick}>{title}</button>)
 }
