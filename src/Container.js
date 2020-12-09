231d08aefee00e5a9e26b10ff0bf0dfe9c0224c0
el1tehax0r <professionalstuff83@gmail.com>
2020-12-09 04:19:48 -0600

    test for ci/cd

--- src/Container.js
+++ src/Container.js
@@ -5,5 +5,6 @@ const Container=({children})=>(
   {children}
   </div>
 )
+//HEY
 
 export default Container;
--- src/FormComponents/Button.js
+++ src/FormComponents/Button.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from '../Stylesheet'
 export default function Button({onClick,title}){
-  //
   return (<button style={styles.button} onClick={onClick}>{title}</button>)
 }
